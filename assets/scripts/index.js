32f6342dda937c630c47fc3ff3216b08163b75ec
miquelmarco <120406764+miquelmarco@users.noreply.github.com>
2024-02-16 12:54:55 -0300

    footer

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,13 +1,11 @@
-// let { createApp } = Vue;
-// createApp({
-//     data() {
-//         return {
-//         }
-//     },
-//     created() {
-//     },
-//     methods: {
-//     },
-//     computed: {
-//     }
-// }).mount("#app")
+// go top
+document.addEventListener('DOMContentLoaded', function () {
+    var goToTopLink = document.getElementById('goToTop')
+    goToTopLink.addEventListener('click', function (event) {
+        event.preventDefault()
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
+})
